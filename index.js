3466895872191c82b3d9eaa79e5a1a526e41158a
Victoria Parry <vlparry@gmail.com>
2022-05-12 12:48:12 -0500

    commit tree 95ded1b59f6f8654c28827f34bc99c844c27e9e9

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const main = main.remove()
+
+// function newHeader (newNode) {
+//     let h1 = document.querySelector(h1)
+//     h1.
+// }
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = "Tor is the champion";
+document.body.append(newHeader)
